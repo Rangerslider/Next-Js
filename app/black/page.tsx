51d12e967754c59f28125438e070b565a7a7436f
Mac User <macuser@MacBook-Pro.local>
2024-07-07 17:47:32 +0600

    first commit

---
+++ app/black/page.tsx
@@ -0,0 +1,15 @@
+const black = () => {
+    return (
+      <div>
+          <h1 style={{ padding:0, width:'300px', height:'90px', margin: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Black Band</h1>
+          <p  style={{ padding:0, width:'600px', height:'auto', margin: 'auto', display: 'flex', textAlign:'justify', justifyContent: 'center', alignItems: 'center' }}>
+          Black is a Bangladeshi alternative rock band formed in 1998 in Dhaka.[1] It was originally formed by Jon Kabir (lead singer, rhythm guitars), Mushfeque Jahan (lead guitars), and Tony Vincent (drums). They were soon joined by Mahmudul Karim Miraz (bass guitars), and in 2000, Tahsan Rahman Khan (keyboards and vocals).[2]
+
+After signing a contract with G-Series, Black broke through the mainstream with their debut studio album Amar Prithibi (2002). Since then, they have released five studio albums and have appeared in some mixed albums. Their latest album (Unomanush) was released in 2016 by G-Series. They were one of key rock bands of the 2000s along with Nemesis and Stentorian, who popularized alternative rock music in Bangladesh.
+Childhood friends and schoolmates Jahangir Jon Kabir, Mushfeque Jahan, and Tony Vincent (Mehmood Afridi Tony) always shared a similar passion for music. Their time was mostly spent at each others houses listening to records from bands such as Pearl Jam, Stone Temple Pilots, Soundgarden. Pearl Jam debut album Ten had a profound effect on the three and is said to have been the tipping point at which they decided to take up music professionally. After convincing each other about their musical skills the trio decided to form a band and soon enough they were jamming at their friend homes. Initially, they decided to name their group Dope Smuglazz, as a wink to the irreverent parental fears of rock music and its supposed concurrent substance abuse, but soon enough shifted to the name Black by a suggestion from a friend, classmate, and future band member Asif Haque. The band would then be introduced to Zubair Hossain Imon, an old acquaintance of guitar player Asif, whom the band considers to be The Stalwart Member and their Philosophical Mentor. He is widely known for helping the band with their songwriting, often directly contributing words and ideas.[3] Black was formed in 1998 with the five members of Jon in vocals, Jahan in guitar, Tony in drums, Asif for additional guitar work, and Tamzid Siddiq Spondon in bass. Soon enough Spondon and Asif parted ways with the band to follow their own interests. Tahsan Rahman Khan then joined the band as the keyboardist in 2000. Their first ever recorded track was Blues n Rod which was released later in a mixed album.
+        </p>
+      </div>
+    );
+  };
+  export default black;
+  
---
+++ app/nagarbaul/page.tsx
@@ -0,0 +1,23 @@
+const nagar = () => {
+  return (
+    <div>
+        <h1 style={{ padding:0, width:'300px', height:'90px', margin: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>NagarBaul</h1>
+        <p  style={{ padding:0, width:'600px', height:'auto', margin: 'auto', textAlign:'justify', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        Nagar Baul started in 1977 and was originally named Feelings and was
+        formed by Kumar Bishwajit. Band members included Ayub Bachchu and
+        James.[9] Ayub Bachchu left the band in 1980.[9] They released their
+        first album, Station Road.[10] James, lead vocal and guitarist,composed
+        all the tracks, and wrote the lyrics for five of them. Though the album
+        was not a hit, the tracks Ager Jonome, Amai Jete Dao and Rupshagor
+        enjoyed moderate success. In 1993, they released their second album Jail
+        Theke Bolchhi. This album was a major hit, and Feelings became a
+        mainstream band.[12] In 1998, they released their fourth album Lais Fita
+        Lais. In 1999, the band was renamed from Feelings to Nagar Baul. In
+        2001, their fifth album was released Dushtur Cheler Dol. This was their
+        first album release under the new band name Nagar Baul, named after
+        their album from 1996.
+      </p>
+    </div>
+  );
+};
+export default nagar;
